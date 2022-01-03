09faabefe7809ced0f52cb401f9511c0e661958e
crgo62 <30781022+crgo62@users.noreply.github.com>
2022-01-03 14:08:30 +0100

    Acc1

--- Proc_02.py
+++ Proc_02.py
@@ -2,3 +2,8 @@ VP = Canvas( window, height=50, width=50, bg="red")
 VP.place(x=10, y= 300)
 VP.create_line(10,10, 40, 40, fill='white')
 VP.create_line(10,40, 40, 10, fill='white', width=5)
+
+Acc_1 = Canvas( window, height=50, width=50, bg="blue")
+Acc_1.place(x=280, y= 300)
+Acc_1.create_line(10,10, 40, 40, fill='white')
+Acc_1.create_line(10,40, 40, 10, fill='white', width=5)
